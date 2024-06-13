5d79868fe0305bc140c64a0c5bde2d4a04a0d543
PerikiyoXD <perikiyoxd@gmail.com>
2024-06-13 18:14:10 +0200

    Use return type on lambda

--- src/SWBF2/Utils.hpp
+++ src/SWBF2/Utils.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
 #include <algorithm>
+#include <string>
 
 namespace SWBF2
 {
     inline void string_tolower(std::string &str)
     {
-        std::transform(str.begin(), str.end(), str.begin(), [](unsigned char c) { return std::tolower(c); });
+        std::transform(str.begin(), str.end(), str.begin(), [](unsigned char c) -> unsigned char { return std::tolower(c); });
     }
 }
